bf2dd1f0994e0f97f9870f63a7e27a5861a0187e
fhiba <fhiba@itba.edu.ar>
2026-03-28 12:01:01 -0300

    Initial commit for ssr next shadcn approuter

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div
+        className="border-4 border-[#1e1b13] px-16 py-10"
+        style={{ boxShadow: '8px 8px 0px 0px #1e1b13' }}
+      >
+        <h1
+          className="text-[7rem] leading-none font-black uppercase tracking-tighter text-[#1e1b13] select-none"
+          style={{ fontFamily: "'Space Grotesk', sans-serif" }}
+        >
+          PLUMBERITO
+        </h1>
+      </div>
+    </div>
+  )
+}
